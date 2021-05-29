171bf933c828d1a417fc6334ada49ac006ffa4a1
Kevin Duconge <64347790+kevduc@users.noreply.github.com>
2021-05-29 04:19:27 +0100

    Added: Clicking on a friend jumps to the person and shows person's friends

--- pages/people/index.jsx
+++ pages/people/index.jsx
@@ -6,6 +6,7 @@ import friendships from 'pages/api/friendships.json'
 
 export default function People() {
   const [friendLists, setFriendLists] = useState(null)
+  const [selectedPerson, setSelectedPerson] = useState(null)
   const [showFriends, setShowFriends] = useState({})
   const [loading, setLoading] = useState(false)
 
@@ -22,45 +23,74 @@ export default function People() {
     fetchFriendLists()
   }, [])
 
-  const handlePersonClick = useCallback(
-    (name) => {
+  const setFriendListDisplay = useCallback(
+    (name, show) => {
       const newShowFriends = Object.assign({}, showFriends)
-      newShowFriends[name] = !newShowFriends[name]
+      newShowFriends[name] = show
       setShowFriends(newShowFriends)
     },
     [showFriends, setShowFriends]
   )
 
+  const handleToggleFriendsDisplay = useCallback(
+    (name) => setFriendListDisplay(name, !showFriends[name]),
+    [setFriendListDisplay, showFriends]
+  )
+
+  const handleFriendClick = useCallback(
+    (name) => {
+      setSelectedPerson(name)
+      setFriendListDisplay(name, true)
+    },
+    [setSelectedPerson, setFriendListDisplay]
+  )
+
   return (
-    <div className="w-100 h-100 d-flex justify-content-center align-items-start mt-5">
-      <div className="w-50">
+    <div className="w-100 h-100 d-flex justify-content-center align-items-start p-5">
+      <div className="">
         {loading && <p className="loading">Loading...</p>}
-        {friendLists && <PeopleList friendLists={friendLists} showFriends={showFriends} onPersonClick={handlePersonClick} />}
+        {friendLists && (
+          <PeopleList
+            friendLists={friendLists}
+            showFriends={showFriends}
+            onToggleFriendsDisplay={handleToggleFriendsDisplay}
+            onFriendClick={handleFriendClick}
+            selectedPerson={selectedPerson}
+          />
+        )}
       </div>
     </div>
   )
 }
 
-function PeopleList({ friendLists, onPersonClick, showFriends }) {
+function PeopleList({ friendLists, onToggleFriendsDisplay, onFriendClick, showFriends, selectedPerson }) {
   return (
     <ul className="list-group">
-      {Object.entries(friendLists).map(([name, friends]) => (
-        <li className="list-group-item" key={name}>
-          <Person onClick={() => onPersonClick(name)} name={name} friends={friends} showFriends={showFriends[name]} />
-        </li>
-      ))}
+      {Object.entries(friendLists).map(([name, friends]) => {
+        const selected = selectedPerson === name
+
+        return (
+          <li className={`list-group-item ${selected ? 'active' : ''}`} key={name}>
+            <Person
+              onToggleFriendsDisplay={() => onToggleFriendsDisplay(name)}
+              onFriendClick={onFriendClick}
+              name={name}
+              friends={friends}
+              showFriends={showFriends[name]}
+            />
+          </li>
+        )
+      })}
     </ul>
   )
 }
 
-function Person({ name, friends, showFriends, onClick }) {
+function Person({ name, friends, showFriends, onFriendClick, onToggleFriendsDisplay }) {
   return (
-    <div className="Person">
+    <div className="person" id={name}>
       <div className="d-flex gap-1">
-        <h2 className="person-name" id={name}>
-          {name}
-        </h2>
-        <button className="btn btn-primary font-monospace m-1" onClick={onClick}>
+        <h2 className="person-name">{name}</h2>
+        <button className="btn btn-secondary font-monospace m-1" onClick={onToggleFriendsDisplay}>
           {showFriends ? '-' : '+'}
         </button>
       </div>
@@ -68,9 +98,13 @@ function Person({ name, friends, showFriends, onClick }) {
         (friends.length === 0 ? (
           <p>No Friends</p>
         ) : (
-          <ul className="list-group list-group-horizontal">
+          <ul className="list-group list-group-horizontal text-center">
             {friends.map((friendName) => (
-              <a key={friendName} href={`#${friendName}`} className="list-group-item list-group-item-action">
+              <a
+                key={friendName}
+                href={`#${friendName}`}
+                onClick={() => onFriendClick(friendName)}
+                className="list-group-item list-group-item-action">
                 {friendName}
               </a>
             ))}
