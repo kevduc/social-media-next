df73b6a9ebf5bee18436264a49e3d8377bf9d5c9
Kevin Duconge <64347790+kevduc@users.noreply.github.com>
2021-05-29 04:44:42 +0100

    Added navbar

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,17 @@ import Link from 'next/link'
 
 export default function IndexPage() {
   return (
-    <div>
-      Social Media{' '}
-      <Link href="/people">
-        <a>People</a>
+    <nav class="navbar py-2 navbar-dark bg-primary">
+      <Link href="/">
+        <a class="navbar-brand mx-4">Social Media</a>
       </Link>
-    </div>
+      <ul class="navbar-nav me-auto">
+        <li class="nav-item mx-2">
+          <Link href="/people">
+            <a class="nav-link active">People</a>
+          </Link>
+        </li>
+      </ul>
+    </nav>
   )
 }
